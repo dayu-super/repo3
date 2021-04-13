7115f361e0875c97f4280de2570b7ab518c6ccb1
dayu <839614342@qq.com>
2021-04-13 16:27:11 +0800

    添加工程到版本库

---
+++ HelloProject/src/com/dayu/hello/Hello.java
@@ -0,0 +1,11 @@
+package com.dayu.hello;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("11111111111");
+        System.out.println("222222222");
+        System.out.println("33333333");
+    }
+
+}
